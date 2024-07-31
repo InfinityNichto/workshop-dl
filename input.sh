ddb210b7cd246844f4edce54c83fd156b4b790ce
ничто <119140957+InfinityNichto@users.noreply.github.com>
2024-07-31 11:18:02 +0700

    Update input.sh

--- input.sh
+++ input.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
 # 105600 for Terraria, 1281930 for TModLoader
 export APP_ID=105600
 export ENTRY_ID=2439880423
+
+# tmod file to extract using extractTmod.yml workflow
+export TMODFILE="weapon.tmod"
