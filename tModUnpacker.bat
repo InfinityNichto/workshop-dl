c41126b06cdf0d1977552ceb53445540387e40d9
ничто <119140957+InfinityNichto@users.noreply.github.com>
2024-07-31 16:47:09 +0700

    Update tModUnpacker.bat

--- tModUnpacker.bat
+++ tModUnpacker.bat
@@ -3,7 +3,7 @@ SET TMODFILE=Weapon.tmod
 :: -------
 
 mkdir "tmodFileExtracted\%TMODFILE%"
-tModUnpacker.exe "tmodFiles\%TMODFILE%" "tmodFileExtracted\%TMODFILE%\"
+tModUnpacker.exe "tmodFile\%TMODFILE%" "tmodFileExtracted\%TMODFILE%\"
 
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
